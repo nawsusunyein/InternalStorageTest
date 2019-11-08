a5be98bb33b4e445310f2ffb8ab65dfc5716e872
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-08 10:55:36 +0630

    add reading data from internal storage file

--- app/src/main/java/com/example/internalstoragetest/DetailsActivity.java
+++ app/src/main/java/com/example/internalstoragetest/DetailsActivity.java
@@ -7,18 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class DetailsActivity extends AppCompatActivity {
 
     private TextView txtUserName;
     private TextView txtPassword;
     private Intent loginIntent;
+    FileInputStream finStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        txtUserName = (TextView) findViewById(R.id.txtUserName);
+        txtPassword = (TextView) findViewById(R.id.txtUserPassword);
+
+        try{
+            finStream = openFileInput("user_details");
+            StringBuffer sbuffer = new StringBuffer();
+            int i;
+            while ((i = finStream.read())!= -1){
+                sbuffer.append((char)i);
+            }
+            finStream.close();
+            String details[] = sbuffer.toString().split("\n");
+            txtUserName.setText(details[0]);
+            txtPassword.setText(details[1]);
+            //result.setText("Name: "+ details[0]+"\nPassword: "+details[1]);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
+    private void setDetailsInfo(){}
     public void goBackToLoginScreen(View view){
         loginIntent = new Intent(DetailsActivity.this,MainActivity.class);
         startActivity(loginIntent);
--- app/src/main/java/com/example/internalstoragetest/MainActivity.java
+++ app/src/main/java/com/example/internalstoragetest/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.internalstoragetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +20,33 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtPassword;
     private Button btnSave;
     private Intent detailsIntent;
+    private FileOutputStream foutStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUsername = (TextView) findViewById(R.id.txtUsername);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
     }
 
     public void saveUserInfoAndGoToDetailsScreen(View view){
-        detailsIntent = new Intent(MainActivity.this,DetailsActivity.class);
-        startActivity(detailsIntent);
+        String username = txtUsername.getText().toString() + "\n";
+        String password = txtPassword.getText().toString();
+
+        try{
+            foutStream = openFileOutput("user_details", Context.MODE_PRIVATE);
+            foutStream.write(username.getBytes());
+            foutStream.write(password.getBytes());
+            Toast.makeText(this,"Details info saved successfully",Toast.LENGTH_LONG).show();
+            detailsIntent = new Intent(MainActivity.this,DetailsActivity.class);
+            startActivity(detailsIntent);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
